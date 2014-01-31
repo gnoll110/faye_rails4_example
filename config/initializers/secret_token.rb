a49f1f450a9bd42480fbd943bf55d5ed89e1175c
Noel Kelly <gnoll110@yahoo.com.au>
2014-01-31 16:18:52 +1000

    rails 4.0.2 new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FayeRails4Example::Application.config.secret_key_base = '0d0d63b30be5fbcc22cbf2fc69115b8306e78c7d5110e082bba92dac059632fcaa0dc1353e15e705f081283e8cb4ca90083ecef7c5706305a92f389dfd7fe5ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FayeRails4Example::Application.config.session_store :cookie_store, key: '_faye_rails4_example_session'
